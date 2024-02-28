1a08c6eff7fd9b71c1e82f07506f5823992d8b53
Jonas Frey <jonfrey@ethz.ch>
2024-02-28 17:26:05 +0100

    black formatting

--- timing.py
+++ timing.py
@@ -7,11 +7,12 @@ from torchvision import datasets, transforms
 from pytictac import Timer, CpuTimer
 import time
 
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3,   1)
-        self.conv2 = nn.Conv2d(  32, 64, 3, 1)
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
         self.dropout1 = nn.Dropout(0.25)
         self.dropout2 = nn.Dropout(0.5)
         self.fc1 = nn.Linear(9216, 128)
@@ -35,13 +36,14 @@ class Net(nn.Module):
 
 def main():
     # Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size',                      type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
+    parser = argparse.ArgumentParser(description="PyTorch MNIST Example")
+    parser.add_argument(
+        "--batch-size", type=int, default=64, metavar="N", help="input batch size for training (default: 64)"
+    )
+    parser.add_argument(
+        "--test-batch-size", type=int, default=1000, metavar="N", help="input batch size for testing (default: 1000)"
+    )
+    parser.add_argument("--no-cuda", action="store_true", default=False, help="disables CUDA training")
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
 
@@ -52,58 +54,52 @@ def main():
     else:
         device = torch.device("cpu")
 
-    train_kwargs = {           'batch_size': args.batch_size}
-    test_kwargs = {'batch_size': args.test_batch_size}
+    train_kwargs = {"batch_size": args.batch_size}
+    test_kwargs = {"batch_size": args.test_batch_size}
     if use_cuda:
-        cuda_kwargs = {'num_workers': 1,
-                       'pin_memory': True,
-                       'shuffle': True}
+        cuda_kwargs = {"num_workers": 1, "pin_memory": True, "shuffle": True}
         train_kwargs.update(cuda_kwargs)
         test_kwargs.update(cuda_kwargs)
 
-    transform=transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
-        ])
-    dataset1 = datasets.MNIST('../data', train=True, download=True,
-                       transform=transform)
-    train_loader = torch.utils.data.DataLoader( dataset1,**train_kwargs)
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+    dataset1 = datasets.MNIST("../data", train=True, download=True, transform=transform)
+    train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
     data, target = next(iter(train_loader))
     data, target = data.to(device), target.to(device)
 
     model = Net().to(device)
 
-    with CpuTimer('cpu timer before warm up'):
+    with CpuTimer("cpu timer before warm up"):
         model(data)
 
-    with Timer('gpu timer before warum up'):
+    with Timer("gpu timer before warum up"):
         model(data)
-    
+
     for i in range(100):
         model(data)
 
-    with Timer('gpu timer after warm up'):
+    with Timer("gpu timer after warm up"):
         model(data)
 
-    with CpuTimer('cpu timer after warm up'):
+    with CpuTimer("cpu timer after warm up"):
         model(data)
 
-    with Timer('100 x gpu timer after warm up'):
+    with Timer("100 x gpu timer after warm up"):
         for i in range(100):
             noise = torch.rand_like(data) * 0.0001
             model(data + noise)
 
-    with CpuTimer('100 x cpu timer after warm up'):
+    with CpuTimer("100 x cpu timer after warm up"):
         for i in range(100):
             noise = torch.rand_like(data) * 0.0001
-            model(data + noise )
+            model(data + noise)
 
-
-    with Timer('100 x gpu timer after warm up - with sync'):
+    with Timer("100 x gpu timer after warm up - with sync"):
         for i in range(100):
             noise = torch.rand_like(data) * 0.0001
             model(data + noise)
             torch.cuda.synchronize()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
