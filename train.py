fbff08841077c438b1b03c564bdba47b614f275b
Jose Lavariega <josea.lavariegago@gmail.com>
2024-03-01 13:34:13 +0100

    Task 0 Complete. Complaining comment to train.py

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 
-
+# Task 0: The accuracy is too low :(
 class Net(nn.Module):
     def __init__(self):
 
