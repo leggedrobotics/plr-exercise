2796f7a7beb3193466df78e22cd56356206d0f7e
Jose Lavariega <josea.lavariegago@gmail.com>
2024-03-02 13:37:52 +0100

    adds wandb. Wandb folder moved to results(ignored)

--- plr_exercise/models/cnn.py
+++ plr_exercise/models/cnn.py
@@ -1,6 +1,13 @@
+from __future__ import print_function
+import argparse
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
+
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+
 
 class Net(nn.Module):
     def __init__(self):
