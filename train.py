c6de2758503b5aa09e480d0b68ead62a8fa4f5ef
Alexandre <alexandre.clin@orange.fr>
2024-03-06 10:50:33 +0100

    task 3 with wandb

--- train.py
+++ train.py
@@ -6,15 +6,25 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
+import wandb
+
+
+run = wandb.init(
+    project="mon projet",
+    config={
+        "learning_rate": 0.01,
+        "epochs": 10,
+
+    },
+)
 
 
 class Net(nn.Module):
     def __init__(self):
 
-
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1,   32, 3, 1)
-        self.conv2 = nn.Conv2d( 32, 64, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
         self.dropout1 = nn.Dropout(0.25)
         self.dropout2 = nn.Dropout(0.5)
         self.fc1 = nn.Linear(9216, 128)
@@ -43,7 +53,7 @@ def train(args,    model, device, train_loader, optimizer, epoch):
         data, target = data.to(device),   target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        loss = F.nll_loss(   output, target)
+        loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
@@ -52,7 +62,7 @@ def train(args,    model, device, train_loader, optimizer, epoch):
                     epoch,
                     batch_idx * len(data),
                     len(train_loader.dataset),
-                    100.0 *    batch_idx / len(train_loader),
+                    100.0 * batch_idx / len(train_loader),
                     loss.item(),
                 )
             )
@@ -70,17 +80,21 @@ def test(model, device, test_loader, epoch):
 
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(   output,   target,   reduction="sum").item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)  ).sum().item()
+            # sum up batch loss
+            test_loss += F.nll_loss(output,   target,   reduction="sum").item()
+            # get the index of the max log-probability
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
-    test_loss /= len(test_loader.dataset  )
+    test_loss /= len(test_loader.dataset)
 
     print(
         "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
-            test_loss, correct, len(test_loader.dataset), 100.0 * correct / len(test_loader.dataset)
+            test_loss, correct, len(
+                test_loader.dataset), 100.0 * correct / len(test_loader.dataset)
         )
     )
+    wandb.log({"Loss": test_loss, "epoch": epoch})
 
 
 def main():
@@ -92,12 +106,18 @@ def main():
     parser.add_argument(
         "--test-batch-size", type=int, default=1000, metavar="N", help="input batch size for testing (default: 1000)"
     )
-    parser.add_argument("--epochs", type=int, default=2, metavar="N", help="number of epochs to train (default: 14)")
-    parser.add_argument("--lr", type=float, default=1.0, metavar="LR", help="learning rate (default: 1.0)")
-    parser.add_argument("--gamma", type=float, default=0.7, metavar="M", help="Learning rate step gamma (default: 0.7)")
-    parser.add_argument("--no-cuda", action="store_true", default=False, help="disables CUDA training")
-    parser.add_argument("--dry-run", action="store_true", default=False, help="quickly check a single pass")
-    parser.add_argument("--seed", type=int, default=1, metavar="S", help="random seed (default: 1)")
+    parser.add_argument("--epochs", type=int, default=2, metavar="N",
+                        help="number of epochs to train (default: 14)")
+    parser.add_argument("--lr", type=float, default=1.0,
+                        metavar="LR", help="learning rate (default: 1.0)")
+    parser.add_argument("--gamma", type=float, default=0.7,
+                        metavar="M", help="Learning rate step gamma (default: 0.7)")
+    parser.add_argument("--no-cuda", action="store_true",
+                        default=False, help="disables CUDA training")
+    parser.add_argument("--dry-run", action="store_true",
+                        default=False, help="quickly check a single pass")
+    parser.add_argument("--seed", type=int, default=1,
+                        metavar="S", help="random seed (default: 1)")
     parser.add_argument(
         "--log-interval",
         type=int,
@@ -105,7 +125,8 @@ def main():
         metavar="N",
         help="how many batches to wait before logging training status",
     )
-    parser.add_argument("--save-model", action="store_true", default=False, help="For Saving the current Model")
+    parser.add_argument("--save-model", action="store_true",
+                        default=False, help="For Saving the current Model")
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
 
@@ -123,8 +144,10 @@ def main():
         train_kwargs.update(cuda_kwargs)
         test_kwargs.update(cuda_kwargs)
 
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-    dataset1 = datasets.MNIST("../data", train=True, download=True, transform=transform)
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+    dataset1 = datasets.MNIST("../data", train=True,
+                              download=True, transform=transform)
     dataset2 = datasets.MNIST("../data", train=False, transform=transform)
     train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
